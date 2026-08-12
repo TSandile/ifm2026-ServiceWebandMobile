ad122fe345f7182962ca13bad7fc4e340f38be64
TSandile <Sandile.Tshabalala11@gmail.com>
2026-08-12 14:26:17 +0200

    modify User entity  for test purpose on docker

--- hfservice/src/main/java/com/mshenguDev/hfservice/entities/User.java
+++ hfservice/src/main/java/com/mshenguDev/hfservice/entities/User.java
@@ -1,4 +1,23 @@
 package com.mshenguDev.hfservice.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Long getId(){return id;}
+    public String getName(){return name;}
 }
