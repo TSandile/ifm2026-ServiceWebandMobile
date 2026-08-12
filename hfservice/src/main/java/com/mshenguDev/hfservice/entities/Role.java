55cdee8c8aed75763dd54ddfbce98007e684b1fb
TSandile <Sandile.Tshabalala11@gmail.com>
2026-08-12 17:12:44 +0200

    update User entinty and added role for multiple actors

--- hfservice/src/main/java/com/mshenguDev/hfservice/entities/Role.java
+++ hfservice/src/main/java/com/mshenguDev/hfservice/entities/Role.java
@@ -1,4 +1,8 @@
 package com.mshenguDev.hfservice.entities;
 
 public enum Role {
+    ADMIN,
+    MANAGER,
+    CLERK,
+    CUSTOMER
 }
--- hfservice/src/main/java/com/mshenguDev/hfservice/entities/User.java
+++ hfservice/src/main/java/com/mshenguDev/hfservice/entities/User.java
@@ -5,19 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Date;
+
 @Entity
+
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private String password;
+    private Integer phone;
+    private Role role;
+    private Boolean enabled;
+    private Date createdAt;
 
-    private String name;
 
-    public void setName(String name){
-        this.name = name;
-    }
-
-    public Long getId(){return id;}
-    public String getName(){return name;}
 }
