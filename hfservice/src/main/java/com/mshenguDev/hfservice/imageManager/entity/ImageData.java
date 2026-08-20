44920b72aca36479f935c1b6d1cb4f11bbbb486f
TSandile <Sandile.Tshabalala11@gmail.com>
2026-08-20 14:47:18 +0200

    Update image entity

--- hfservice/src/main/java/com/mshenguDev/hfservice/imageManager/entity/ImageData.java
+++ hfservice/src/main/java/com/mshenguDev/hfservice/imageManager/entity/ImageData.java
@@ -1,4 +1,29 @@
 package com.mshenguDev.hfservice.imageManager.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ImageData")
 public class ImageData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+
+    @Lob
+    @Column(name = "image_data", length = 1000)
+    private byte[] imageData;
+
+    public ImageData(){}
+    public ImageData(String name, String type){
+        this.name = name;
+        this.type = type;
+    }
+
+    public void setName(String name){ this.name = name;}
+    public void setType(String type){this.type = type;}
+
+    public String getName(){return name;}
+    public String getType(){return type;}
 }
